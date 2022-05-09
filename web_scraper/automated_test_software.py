397bcda80667fb28b34325ebdb35232c166f3230
justine.lim <justine.lim@yiducloud.cn>
2022-05-09 10:26:34 +0800

    Delete automated_test_software.py

--- web_scraper/automated_test_software.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-browser = webdriver.Chrome("chromedriver")
-browser.get("https://www.morningstar.com/")
-input = browser.find_element_by_css_selector(".mdc-search-field__input")
-input.send_keys("example text")
